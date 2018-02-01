e30219782db3c592dd91efacdb784f4fcc93903d
Erik Escoffier <erik.escoffier@gmail.com>
2018-02-01 17:48:37 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+/* global document */
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import MapGL from 'react-map-gl';
+import DeckGLOverlay from './deckgl-overlay';
+import MAPBOX_TOKEN from './mapbox-token';
+
+class Root extends Component {
+  constructor(props) {
+    super(props);
+
+    const data = Array(1000000).fill(null).map(() => {
+      return {
+        radius: 5,
+        color: [255, 0, 0],
+        position: [
+          -45 + Math.random() * 90,
+          -45 + Math.random() * 90,
+        ]
+      };
+    });
+
+    this.state = {
+      viewport: {
+        width: 1000,
+        height: 800,
+        latitude: 0,
+        longitude: 0,
+        zoom: 5,
+        bearing: -20.55991,
+        pitch: 60
+      },
+      data
+    };
+
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resize.bind(this));
+    this._resize();
+  }
+
+  _resize() {
+    this._onViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
+  _onViewportChange(viewport) {
+    this.setState({
+      viewport: {...this.state.viewport, ...viewport}
+    });
+  }
+
+  render() {
+    const {viewport, data} = this.state;
+    // const l = -15 + Math.random() * 30;
+    // const filteredData = data.filter(d => d.position[0] > l);
+    return (
+      <MapGL
+        {...viewport}
+        onViewportChange={this._onViewportChange.bind(this)}
+        mapboxApiAccessToken={MAPBOX_TOKEN}>
+        <DeckGLOverlay viewport={viewport} data={data || []} />
+      </MapGL>
+    );
+  }
+}
+
+render(<Root />, document.body.appendChild(document.createElement('div')));
+
---
+++ deckgl-overlay.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import DeckGL, {LineLayer, ScatterplotLayer} from 'deck.gl';
+
+export default class DeckGLOverlay extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      r: 400
+    };
+    this._tick = this._tick.bind(this);
+  }
+
+  componentDidMount() {
+    this._animate();
+  }
+
+  _animate() {
+    // this._stopAnimate();
+
+    this.intervalTimer = window.setInterval(this._tick, 20);
+  }
+
+  _tick() {
+    this.setState({r : this.state.r + 10});
+  }
+
+  render() {
+    const {viewport, data} = this.props;
+    const {r} = this.state;
+    console.log(r)
+    return (
+      <DeckGL
+        {...viewport}
+        debug
+        layers={[
+          new LineLayer({
+            data: [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}]
+          }),
+          new ScatterplotLayer({
+            id: 'scatterplot-layer',
+            data,
+            radiusScale: 400,
+            outline: false,
+            opacity: .5
+          })
+        ]} />
+    );
+  }
+}
