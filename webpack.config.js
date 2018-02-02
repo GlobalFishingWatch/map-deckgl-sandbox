98dae520ca1387589cd20be0b1947503d1ff30b1
Erik Escoffier <erik.escoffier@gmail.com>
2018-02-02 11:21:37 +0100

    replace buble with babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,18 +10,31 @@ const CONFIG = {
   devtool: 'source-map',
 
   module: {
-    rules: [{
-      // Compile ES2015 using buble
-      test: /\.js$/,
-      loader: 'buble-loader',
-      include: [resolve('.')],
-      exclude: [/node_modules/],
-      options: {
-        objectAssign: 'Object.assign',
-        transforms: { dangerousForOf: true },
-      },
-
-    }]
+    rules: [
+      // {
+      //   // Compile ES2015 using buble
+      //   test: /\.js$/,
+      //   loader: 'buble-loader',
+      //   include: [resolve('.')],
+      //   exclude: [/node_modules/],
+      //   options: {
+      //     objectAssign: 'Object.assign'
+      //   },
+      // },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            // options: {
+            //   plugins: ['lodash'],
+            //   presets: [['env', { modules: false, targets: { node: 4 } }]]
+            // }
+          }
+        ]
+      }
+    ]
   },
 
   resolve: {
